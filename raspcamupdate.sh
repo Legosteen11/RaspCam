4308e01fccea82218d61a6857e0d602e17c0a62f
Wouter Doeland <wouterdoeland@gmail.com>
2014-08-08 19:36:37 +0200

    Update raspcamupdate.sh

--- raspcamupdate.sh
+++ raspcamupdate.sh
@@ -7,4 +7,4 @@ wget https://raw.githubusercontent.com/Legosteen11/RaspCam/gh-pages/raspcam.sh
 sudo chmod +x raspcam.sh
 mv raspcam.sh /usr/bin/raspcam
 mv raspcaminstaller.sh /usr/bin/raspcamupdate
-echo "Done installing. You can start RaspCam by typing 'raspcam' And update by typing 'raspcamupdate'"
+echo "Done installing. You can start RaspCam by typing 'sudo raspcam' And update by typing 'sudo raspcamupdate'"
