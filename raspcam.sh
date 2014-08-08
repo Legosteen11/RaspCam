9a5ac988559eb6ed77c03c368daf71130a659f0f
Wouter Doeland <wouterdoeland@gmail.com>
2014-08-08 15:56:56 +0200

    Update raspcam.sh

--- raspcam.sh
+++ raspcam.sh
@@ -8,8 +8,8 @@ sleep 5
 echo "Taking picture..."
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-fswebcam /var/www/html/$DATE.jpg
-fswebcam /var/www/html/camera.jpg
+fswebcam /var/www/$DATE.jpg
+fswebcam /var/www/camera.jpg
 echo "Picture named to: "
 echo $DATE
 
