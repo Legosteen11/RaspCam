6a61351c06168412846a034f2d2d2a0a50c33571
Wouter Doeland <wouterdoeland@gmail.com>
2014-08-08 19:15:52 +0200

    Create raspcamupdate.sh

---
+++ raspcamupdate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Removing old raspcam"
+sudo rm /usr/bin/raspcamupdate
+sudo rm /usr/bin/raspcam
+sudo rm raspcam.sh
+wget https://raw.githubusercontent.com/Legosteen11/RaspCam/gh-pages/raspcam.sh
+sudo chmod +x raspcam.sh
+mv raspcam.sh /usr/bin/raspcam
+mv raspcaminstaller.sh /usr/bin/raspcamupdate
+echo "Done installing. You can start RaspCam by typing 'raspcam' And update by typing 'raspcamupdate'"
