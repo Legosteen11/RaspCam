2970e322e20b06da7d843b990a48f55ee2f8b139
Wouter Doeland <wouterdoeland@gmail.com>
2014-08-08 19:18:58 +0200

    Update raspcaminstaller.sh

--- raspcaminstaller.sh
+++ raspcaminstaller.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Removing old raspcam"
+sudo rm raspcamupdate.sh
 sudo rm /usr/bin/raspcamupdate
 sudo rm /usr/bin/raspcam
 sudo rm raspcam.sh
@@ -8,9 +9,13 @@ sudo apt-get install apache2 php5 libapache2-mod-php5
 sudo service apache2 restart
 echo "Done! Installing fswebcam"
 sudo apt-get install fswebcam
+echo "Done! Downloading RaspCam"
 wget https://raw.githubusercontent.com/Legosteen11/RaspCam/gh-pages/raspcam.sh
+wget https://raw.githubusercontent.com/Legosteen11/RaspCam/gh-pages/raspcamupdate.sh
+sudo chmod +x raspcamupdate.sh
 sudo chmod +x raspcam.sh
-mv raspcam.sh /usr/bin/raspcam
-mv raspcaminstaller.sh /usr/bin/raspcamupdate
+sudo mv raspcamupdate.sh /usr/bin/raspcamupdate
+sudo mv raspcam.sh /usr/bin/raspcam
+sudo mv raspcamupdate.sh /usr/bin/raspcamupdate
 sudo chown -R $USER /var/www
-echo "Done installing. You can start RaspCam by typing 'raspcam' And update by typing 'raspcamupdate'"
+echo "Done installing. You can start RaspCam by typing 'raspcam' And update by typing 'raspcamupdate'(Must be run as root)"
