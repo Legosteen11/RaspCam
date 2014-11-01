b37957ce1dd10f6140d5ae76f996d6887c181a92
Wouter Doeland <wouterdoeland@gmail.com>
2014-11-01 19:49:56 +0100

    New Raspcam, thanks to Taronyu

---
+++ raspcam.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Credits to taronyu for new installer!
+# INSTALL THIS SCRIPT USING:
+# wget https://raw.githubusercontent.com/Taronyuu/RaspCam/gh-pages/raspcam.sh && sudo chmod +x raspcam.sh && mv raspcam.sh /usr/bin/raspcam
+#
+# To update run:
+# raspcam install
+
+if [ $1 == install ]; then
+	echo "Installing webserver"
+	sudo apt-get install apache2 php5 libapache2-mod-php5
+	sudo service apache2 restart
+	echo "Done! Installing fswebcam"
+	sudo apt-get install fswebcam
+	echo "Removing old raspcam"
+	sudo rm /usr/bin/raspcam
+	sudo rm raspcam.sh
+	wget https://raw.githubusercontent.com/Legosteen11/RaspCam/gh-pages/raspcam.sh
+	sudo chmod +x raspcam.sh
+	mv raspcam.sh /usr/bin/raspcam
+	echo "Done installing. You can start RaspCam by typing 'sudo raspcam' And update by typing 'sudo raspcam install'"
+else
+	if [ -a /usr/bin/raspcam ]; then
+		i="0"
+
+		while [ $i -lt 4 ]; do
+
+		sleep 5
+		echo "Taking picture..."
+		DATE=$(date +"%Y-%m-%d_%H:%M:%s")
+
+		fswebcam /var/www/$DATE.jpg
+		fswebcam /var/www/camera.jpg
+		echo "Picture named to: "
+		echo $DATE
+		echo "Press Ctrl + C, to stop script."
+
+		done
+	else
+		echo "Installing webserver"
+		sudo apt-get install apache2 php5 libapache2-mod-php5
+		sudo service apache2 restart
+		echo "Done! Installing fswebcam"
+		sudo apt-get install fswebcam
+		echo "Removing old raspcam"
+		sudo rm /usr/bin/raspcam
+		sudo rm raspcam.sh
+		wget https://raw.githubusercontent.com/Legosteen11/RaspCam/gh-pages/raspcam.sh
+		sudo chmod +x raspcam.sh
+		mv raspcam.sh /usr/bin/raspcam
+		echo "Done installing. You can start RaspCam by typing 'raspcam' And update by typing 'sudo raspcam install'"
+	fi
+fi
